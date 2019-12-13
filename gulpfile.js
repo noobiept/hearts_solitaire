69d05dcd0cbad0459fa99a51cd8e02cc0608f8a3
Pedro Ferreira <plferreira4@gmail.com>
2019-12-13 19:07:02 +0000

    Fix the build release script.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 const { src, dest, series } = require("gulp");
 const rimraf = require("rimraf");
+const fs = require("fs");
+const merge = require("merge-stream");
+const path = require("path");
+
+const PACKAGE = JSON.parse(fs.readFileSync("./package.json"));
+const RELEASE_PATH = `release/${PACKAGE.name} ${PACKAGE.version}`;
 
 function cleanLibrariesDirectory(cb) {
     rimraf("libraries/**", () => {
@@ -15,4 +21,23 @@ function copyLibraries(cb) {
     ]).pipe(dest("libraries/"));
 }
 
+function cleanReleaseDirectory(cb) {
+    rimraf(RELEASE_PATH + "/**", () => {
+        cb();
+    });
+}
+
+function copyReleaseFiles(cb) {
+    const paths = [
+        { src: "build/**", dest: path.join(RELEASE_PATH, "/build") },
+        { src: "libraries/**", dest: path.join(RELEASE_PATH, "/libraries") },
+        { src: "images/**", dest: path.join(RELEASE_PATH, "/images") },
+        { src: "index.html", dest: RELEASE_PATH },
+    ];
+
+    var tasks = paths.map((path) => src(path.src).pipe(dest(path.dest)));
+    return merge(tasks);
+}
+
 exports.update_libraries = series(cleanLibrariesDirectory, copyLibraries);
+exports.release = series(cleanReleaseDirectory, copyReleaseFiles);
